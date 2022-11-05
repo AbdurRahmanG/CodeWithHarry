d50c2e0a1987c9bc46f20f51b3f4951fc5abfad1
AbdurRahman G <abdurrahmangofficial@gmail.com>
2022-11-05 11:24:08 +0530

    Data Types and Objects

---
+++ javascript/data-types-and-objects/script.js
@@ -0,0 +1,30 @@
+// Data Tyes
+
+// Primitive Data Types
+
+// nn bb ss u
+let a = null;
+let b = 1998;
+let c = true;
+let d = BigInt("15101998");
+let e = `AbdrRahman G`;
+// let f = Symbol();
+let g = undefined;
+// or
+let h;
+console.log(`${a} ${b} ${c} ${d} ${e} ${g} ${h}`);
+
+
+// Non-Primitive Data Types
+
+// Objects (Key Value Pairs)
+const studentAbdurRahmanG = {
+    "Name": "AbdurRahman G",
+    "Roll No.": 1,
+    "DOB": "October 15, 1998",
+    "School": "S.S. High School, Bhandar, Bihar",
+    "College": "H.S.B.T.E, Panchkula, Haryana"
+}
+console.log(studentAbdurRahmanG["Name"]);
+console.log(studentAbdurRahmanG["Roll No."]);
+console.log(studentAbdurRahmanG["DOB"]);
