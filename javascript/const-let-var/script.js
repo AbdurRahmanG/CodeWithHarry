7f8bff58677a55cfdc2b202fe057b7910a251b28
AbdurRahman G <abdurrahmangofficial@gmail.com>
2022-11-05 10:38:26 +0530

    const, let, and var

---
+++ javascript/const-let-var/script.js
@@ -0,0 +1,7 @@
+var globalVariable = `It's a global level scope variable.`;
+let blockVariable = `It's a block level scope variable.`;
+const constantValue = `It's a constant value which can't be changed later.`
+
+let space = ` `;
+
+console.log(globalVariable + space + blockVariable + space + constantValue);
